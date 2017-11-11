0b221a17137753e520472c082706192539d1fe60
pankaj <pankaj@192.168.1.2>
2017-11-11 19:33:21 +0530

    changed pom

--- maven001/src/test/java/CucumberJava/cucumberJava.java
+++ maven001/src/test/java/CucumberJava/cucumberJava.java
@@ -4,9 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver; 
 import org.openqa.selenium.firefox.FirefoxDriver; 
 
-import cucumber.annotation.en.Given; 
-import cucumber.annotation.en.Then; 
-import cucumber.annotation.en.When; 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+//import cucumber.annotation.en.Given; 
+//import cucumber.annotation.en.Then; 
+//import cucumber.annotation.en.When; 
 
 public class cucumberJava { 
    WebDriver driver = null; 
--- maven001/src/test/java/CucumberJava/runTest.java
+++ maven001/src/test/java/CucumberJava/runTest.java
@@ -1,9 +1,34 @@
 package CucumberJava;
- 
-import org.junit.runner.RunWith; 
-import cucumber.junit.Cucumber; 
 
-@RunWith(Cucumber.class) 
-@Cucumber.Options(format = {"pretty", "html:target/cucumber"}) 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+//import org.junit.runner.RunWith; 
+//import cucumber.junit.Cucumber; 
+//
+//@RunWith(Cucumber.class) 
+//@Cucumber.Options(format = {"pretty", "html:target/cucumber"}) 
+//
+//public class runTest { }
+//package CucumberJava;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+//import org.junit.runner.RunWith; 
+//import cucumber.junit.Cucumber; 
+//
+//@RunWith(Cucumber.class) 
+//@Cucumber.Options(format = {"pretty", "html:target/cucumber"}) 
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber-report.json", "html:target/cucumber-htmlreport"},
+  features = {"src/test/java/CucumberJava"},
+  monochrome=true
+  ) 
+
 
 public class runTest { }
