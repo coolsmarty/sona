fba44449a2f1a3559307a7be4ab674c8b15f3115
coolsmarty <madurimodi88@gmail.com>
2017-11-12 15:35:22 +0530

    screen

--- maven001/src/test/java/CucumberJava/cucumberJava.java
+++ maven001/src/test/java/CucumberJava/cucumberJava.java
@@ -1,6 +1,12 @@
 package CucumberJava; 
 
-import org.openqa.selenium.By; 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver; 
 import org.openqa.selenium.firefox.FirefoxDriver; 
 
